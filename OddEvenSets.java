5d156ceb67f57f778d91dd667f570182d369267d
Boltcrasher <Boltcrasher@gmail.com>
2023-09-25 12:54:55 -0500

    oddeven

--- OddEvenSets.java
+++ OddEvenSets.java
@@ -12,12 +12,19 @@ public class OddEvenSets
 	private Set<Integer> odds;
 	private Set<Integer> evens;
 
-	public OddEvenSets()
-	{
-	}
-
 	public OddEvenSets(String line)
 	{
+		String[] words = line.split(" ");
+		odds = new TreeSet<Integer>();
+		evens = new TreeSet<Integer>();
+		for (String word : words) {
+			int n = Integer.parseInt(word);
+			if (n % 2 == 0) {
+				evens.add(n);
+			} else {
+				odds.add(n);
+			}
+		}
 	}
 
 	public String toString()
